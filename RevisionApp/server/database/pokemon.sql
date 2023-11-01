1c69b06d5a7614cea9f369ef1fc46923b6404aeb
Bee <amy.bee.vanzyl@gmail.com>
2023-11-01 10:03:27 +0000

    gen1 poke

--- RevisionApp/server/database/pokemon.sql
+++ RevisionApp/server/database/pokemon.sql
@@ -7,6 +7,7 @@ CREATE TABLE pokemon (
     evolution_stage VARCHAR(50),  -- 'baby', 'middle', 'final'
     evolves_to INT,               -- Reference to the ID of the evolved form
     study_time INT,              -- Time in minutes
-    image_url VARCHAR(500),
+    sprite_url VARCHAR(500),
+    3D_url VARCHAR(500),
     FOREIGN KEY (evolves_to) REFERENCES pokemon(pokemon_id)
 );
