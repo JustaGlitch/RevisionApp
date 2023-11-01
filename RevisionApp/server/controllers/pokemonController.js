7c060d9d46aa41c3eda7a77fc751a10e3d9aa5e6
Bee <amy.bee.vanzyl@gmail.com>
2023-11-01 11:09:50 +0000

    check

--- RevisionApp/server/controllers/pokemonController.js
+++ RevisionApp/server/controllers/pokemonController.js
@@ -1,11 +1,19 @@
 const Pokemon = require('../models/Pokemon')
 
 async function newBaby (req,res) {
-
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 async function evolution (req,res) {
-
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 module.exports = {newBaby, evolution}
