7d1cd3b64a40369450d52400f57c0a486d2d9995
khalifa <oriyomibamikole@gmail.com>
2023-11-02 18:22:05 +0000

    dynamically rendering the collection page

--- RevisionApp/client/pages/CollectionPage/index.jsx
+++ RevisionApp/client/pages/CollectionPage/index.jsx
@@ -2,38 +2,69 @@ import React, { useState, useEffect } from "react";
 
 function index() {
   const [pokemonData, setPokemonData] = useState({});
+  const [collection, setCollection] = useState([]);
+
   useEffect(() => {
-    const getPokemonInfo = async (id) => {
+    const fetchCollection = async () => {
       try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        const result = await response.json();
-        setPokemonData((prevData) => ({
-          ...prevData,
-          [id]: result,
-        }));
+        const token = localStorage.getItem("token");
+        const response = await fetch(
+          "https://studydex.onrender.com/student/pokemon/collection",
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const collectionData = await response.json();
+        setCollection(collectionData);
+        console.log(collectionData);
       } catch (error) {
-        console.error(error);
+        console.error("Error fetching collection:", error);
       }
-    }
-    for (let id = 1; id <= 18; id++) {
-      getPokemonInfo(id);
-    }
+    };
+
+    fetchCollection();
+
+    // const getPokemonInfo = async (id) => {
+    //   try {
+    //     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    //     const result = await response.json();
+    //     setPokemonData((prevData) => ({
+    //       ...prevData,
+    //       [id]: result,
+    //     }));
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // };
+    // for (let id = 1; id <= 18; id++) {
+    //   getPokemonInfo(id);
+    // }
   }, []);
 
   return (
     <div>
-      <h1>My Collection (<span id='pokemonQuantity'>18</span>/510)</h1>
+      <h1>
+        My Collection (<span id="pokemonQuantity">{collection.length}</span>
+        /510)
+      </h1>
       <div className="d-flex flex-wrap">
-      {Object.values(pokemonData).map((pokemon, index) => (
-        <div className="px-4 mb-4 d-flex flex-column align-items-center" key={index}>
-            <img src={pokemon.sprites.front_default}/>
-            {pokemon.name}
-
-            </div>
-      ))}
+        {collection.map((pokemon) => (
+          <div
+            className="px-4 mb-4 d-flex flex-column align-items-center"
+            key={pokemon.collection_id}
+          >
+            <img src={pokemon.sprite_url} alt={pokemon.name} />
+            <span>{pokemon.name}</span>
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default index;
