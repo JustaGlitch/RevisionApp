1f3bf85c453ad366447e2f1839a7391a63b75605
Milanych <vadimzotov.uk@gmail.com>
2023-11-02 15:48:26 +0000

    added registration s**t

--- RevisionApp/client/components/RegisterForm/RegisterForm.jsx
+++ RevisionApp/client/components/RegisterForm/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -6,10 +6,27 @@ import { useNavigate } from "react-router-dom";
 function RegisterForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [pokemons, setPokemons] = useState([]);
+  const [fieldsValidated, setFieldsValidated] = useState(false);
+  const [addPokemon, setAddPokemon] = useState({});
+  const [selected, setSelected] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState("");
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
+  
+  
+  const handlePokemonClick = (selectedPokemon) => {
+    setSelected(selectedPokemon);
 
+  }; 
+useEffect(() => {
+  if (selected) {
+    setAddPokemon(selected);
+  }
+}, [selected]);
+  useEffect(() => {
+    setFieldsValidated(username !== "" && password !== "" && confirmPassword !== "" && password === confirmPassword);
+  }, [username, password, confirmPassword]);
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (password !== confirmPassword) {
@@ -26,7 +43,6 @@ function RegisterForm() {
         }
       );
       login(response.data.token);
-      navigate("/");
     } catch (error) {
       console.error(
         "Registration failed:",
@@ -34,9 +50,43 @@ function RegisterForm() {
       );
     }
   };
+  const handleSubmitPokemon = async (e) => {
+    e.preventDefault();
+    addChosenPokemon(addPokemon.id)
+    console.log(addPokemon.id)
+    window.location.href = '/'
+  }
+  const fetchPokemons = async () => {
+    const pokemonNames = [3, 6, 9];
+    const pokemonData = [];
+    
+    for (const id of pokemonNames) {
+      const resp = await fetch(`https://studydex.onrender.com/student/pokemon/${id}`);
+      const result = await resp.json();
+      const pokemon = { name: result.name, sprite_url: result.sprite_url, id: result.pokemon_id };
+      pokemonData.push(pokemon);
+    }
+    setPokemons(pokemonData);
+  };
+  const addChosenPokemon = async (id) => {
+    const resp = await fetch(`https://studydex.onrender.com/student/pokemon/new/${id}`, {
+      method: 'POST',
+      headers: {
+        
+        'Authorization': localStorage.getItem('token')
+      }
+    })
+    const result = await resp.json();
+    return result;
+  }
+  
+  useEffect(() => {
+    fetchPokemons()
+    }, [])
+    
 
-  return (
-
+    return (
+    <>
     <form
       className="w-75 p-4 bg-body-secondary shadow rounded"
       onSubmit={handleSubmit}
@@ -54,6 +104,7 @@ function RegisterForm() {
             placeholder="Choose a Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            required
           />
         </div>
         <div className="mb-3">
@@ -67,6 +118,7 @@ function RegisterForm() {
             placeholder="Create a Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
         </div>
         <div className="mb-3">
@@ -80,16 +132,48 @@ function RegisterForm() {
             placeholder="Confirm Your Password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
+            required
           />
         </div>
         <button
           type="submit"
           className="btn btn-info offset-3 col-6 text-white"
+          data-bs-target={fieldsValidated ? "#chooseYourPokemon" :''}
+          data-bs-toggle="modal" 
         >
-          Register
+          Next Step
         </button>
       </div>
     </form>
+    <div className="modal fade" id="chooseYourPokemon" aria-hidden="true" aria-labelledby="chooseYourPokemonLabel" tabIndex="-1">
+  <div className="modal-dialog modal-dialog-centered">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h1 className="modal-title fs-5" id="chooseYourPokemonLabel">Choose your fighter!</h1>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form onSubmit={handleSubmitPokemon}>
+      <div className="modal-body">
+        <p>You have to choose one of starting pokemon.</p>
+        <div className="row">
+          {pokemons.map((pokemon) => (
+          <div className="col" key={pokemon.id}>
+            <div id={pokemon.id} className={`${selected && selected.id === pokemon.id ? 'selected' : ''} d-flex align-items-center flex-column`} onClick={() => handlePokemonClick(pokemon)}>
+              <img src={pokemon.sprite_url} alt={pokemon.name} />
+              <p>{pokemon.name}</p>
+            </div>
+          </div>
+          ))}
+        </div>
+      </div>
+      <div className="modal-footer d-flex justify-content-center">
+        <button className="btn btn-info text-white" type="submit">Submit</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+</>
   );
 }
 
